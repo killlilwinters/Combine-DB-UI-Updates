101ba4aa9adc72f1450e40167ced39af0408a40d
MaksGorobets <gorobets542@gmail.com>
2025-04-13 13:19:47 +0300

    Initial Commit

---
+++ Combine-DB-UI-Updates/Combine_DB_UI_UpdatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_DB_UI_UpdatesApp.swift
+//  Combine-DB-UI-Updates
+//
+//  Created by Maks Winters on 13.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Combine_DB_UI_UpdatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
